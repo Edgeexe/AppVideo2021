f6d4faaf7ed7d26f9a775585c64aed00aab6482b
chifl <chifletero9@hotmail.com>
2021-11-28 19:41:05 +0100

    Ventana principal mayormente estilizada y añadidos iconos de los botones

--- AppVideo/src/main/java/umu/tds/AppVideo/GUI/VentanaPrincipal.java
+++ AppVideo/src/main/java/umu/tds/AppVideo/GUI/VentanaPrincipal.java
@@ -1,93 +1,63 @@
 package umu.tds.AppVideo.GUI;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.Rectangle;
 import java.awt.Color;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.UIManager;
 import javax.swing.JTextField;
 import java.awt.SystemColor;
-import javax.swing.AbstractAction;
-import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
-import javax.swing.Action;
-import javax.swing.BoxLayout;
-import java.awt.Component;
-import javax.swing.border.EmptyBorder;
 import javax.swing.border.BevelBorder;
-import javax.swing.border.MatteBorder;
-import java.awt.event.ActionListener;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import java.awt.Insets;
+
 
 public class VentanaPrincipal extends JFrame {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private JTextField textField;
+	private int posX=0;
+	private int	posY=0;
 
 	public static void main(String[] args) {
 
-        JFrame frame = new VentanaPrincipal(); // GUI gui = new GUI() as well
-        // default value JFrame.HIDE_ON_CLOSE
+        JFrame frame = new VentanaPrincipal();
         frame.setVisible(true);
 }
 	
 	public VentanaPrincipal() {
-		setBounds(new Rectangle(250, 250, 810, 700));
-		setResizable(false);
+		setBounds(new Rectangle(100, 100, 1280, 720));
 		setLocationRelativeTo(null);
+		setUndecorated(true);
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
 		
 		JPanel panel_cabecera = new JPanel();
-		panel_cabecera.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_cabecera.setBounds(10, 0, 774, 67);
+		panel_cabecera.setOpaque(false);
+		panel_cabecera.setBorder(null);
+		panel_cabecera.setBounds(0, 0, 1280, 95);
 		getContentPane().add(panel_cabecera);
 		panel_cabecera.setLayout(null);
 		
-		JButton boton_Registro = new JButton("Registro");
-		boton_Registro.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if(e.getButton()==MouseEvent.BUTTON1) {
-					JFrame registro=new SignUp();
-					registro.setVisible(true);
-					dispose();
-				}
-			}
-		});
-		boton_Registro.setBounds(381, 11, 90, 29);
-		panel_cabecera.add(boton_Registro);
-		
-		JButton boton_Login = new JButton("Login");
-		boton_Login.setBounds(469, 11, 71, 29);
-		panel_cabecera.add(boton_Login);
-		boton_Login.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if(e.getButton()==MouseEvent.BUTTON1) {
-					JFrame login=new Login();
-					login.setVisible(true);
-					dispose();
-				}
-			}
-		});
-		
 		JButton boton_Logout = new JButton("Logout");
-		boton_Logout.setBounds(569, 11, 79, 29);
+		boton_Logout.setBounds(1081, 33, 79, 29);
 		panel_cabecera.add(boton_Logout);
 		
 		JButton boton_Premium = new JButton("Premium");
-		boton_Premium.setBounds(679, 11, 90, 29);
+		boton_Premium.setBounds(1170, 33, 90, 29);
 		boton_Premium.setForeground(Color.RED);
 		panel_cabecera.add(boton_Premium);
 		
 		textField = new JTextField();
-		textField.setBounds(165, 3, 223, 37);
+		textField.setBounds(848, 29, 223, 37);
 		textField.setText("Hola + nombre de usuario");
 		textField.setSelectionColor(SystemColor.menu);
 		textField.setEditable(false);
@@ -96,58 +66,135 @@ public class VentanaPrincipal extends JFrame {
 		textField.setBackground(SystemColor.menu);
 		panel_cabecera.add(textField);
 		
+		JLabel label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/src/main/resources/LogoNoText.png")));
+		label_1.setBounds(56, 11, 100, 95);
+		panel_cabecera.add(label_1);
+		
 		JPanel panel_botones = new JPanel();
+		panel_botones.setOpaque(false);
 		panel_botones.setBorder(null);
-		panel_botones.setBounds(0, 69, 794, 29);
+		panel_botones.setBounds(0, 106, 206, 614);
 		getContentPane().add(panel_botones);
 		panel_botones.setLayout(null);
 		
-		JButton boton_Explorar = new JButton("Explorar");
-		boton_Explorar.setBounds(10, 1, 105, 26);
+		JButton boton_Explorar = new JButton("        Explorar");
+		boton_Explorar.setMargin(new Insets(2, 25, 2, 14));
+		boton_Explorar.setRequestFocusEnabled(false);
+		boton_Explorar.setHorizontalAlignment(SwingConstants.LEFT);
+		boton_Explorar.setContentAreaFilled(false);
+		boton_Explorar.setBorderPainted(false);
+		boton_Explorar.setOpaque(false);
+		boton_Explorar.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/src/main/resources/HomeIcon.png")));
+		boton_Explorar.setFont(new Font("Lato Black", Font.BOLD, 15));
+		boton_Explorar.setForeground(Color.WHITE);
+		boton_Explorar.setBounds(0, 139, 206, 46);
 		panel_botones.add(boton_Explorar);
 		
-		JButton boton_mis_listas = new JButton("Mis Listas");
-		boton_mis_listas.setBounds(115, 1, 105, 26);
+		boton_Explorar.addMouseListener(new MouseAdapter() {
+		    public void mouseEntered(MouseEvent evt) {
+		    	boton_Explorar.setContentAreaFilled(true);
+		    	boton_Explorar.setBackground(new Color(41,41,41));
+		    }
+
+		    public void mouseExited(MouseEvent evt) {
+		    	boton_Explorar.setContentAreaFilled(false);		    }
+		});
+		
+		JButton boton_mis_listas = new JButton("        Mis Listas");
+		boton_mis_listas.setMargin(new Insets(2, 25, 2, 14));
+		boton_mis_listas.setOpaque(false);
+		boton_mis_listas.setRequestFocusEnabled(false);
+		boton_mis_listas.setHorizontalAlignment(SwingConstants.LEFT);
+		boton_mis_listas.setContentAreaFilled(false);
+		boton_mis_listas.setBorderPainted(false);
+		boton_mis_listas.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/src/main/resources/LibraryIcon.png")));
+		boton_mis_listas.setForeground(Color.WHITE);
+		boton_mis_listas.setFont(new Font("Lato Black", Font.BOLD, 15));
+		boton_mis_listas.setBounds(0, 185, 206, 46);
 		panel_botones.add(boton_mis_listas);
 		
-		JButton boton_Recientes = new JButton("Recientes");
-		boton_Recientes.setBounds(220, 1, 105, 26);
+		boton_mis_listas.addMouseListener(new MouseAdapter() {
+		    public void mouseEntered(MouseEvent evt) {
+		    	boton_mis_listas.setContentAreaFilled(true);
+		    	boton_mis_listas.setBackground(new Color(41,41,41));
+		    }
+
+		    public void mouseExited(MouseEvent evt) {
+		    	boton_mis_listas.setContentAreaFilled(false);		    
+		    }
+		});
+		
+		JButton boton_Recientes = new JButton("        Recientes");
+		boton_Recientes.setMargin(new Insets(2, 25, 2, 14));
+		boton_Recientes.setOpaque(false);
+		boton_Recientes.setRequestFocusEnabled(false);
+		boton_Recientes.setHorizontalAlignment(SwingConstants.LEFT);
+		boton_Recientes.setContentAreaFilled(false);
+		boton_Recientes.setBorderPainted(false);
+		boton_Recientes.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/src/main/resources/RecentsIcon.png")));
+		boton_Recientes.setForeground(Color.WHITE);
+		boton_Recientes.setFont(new Font("Lato Black", Font.BOLD, 15));
+		boton_Recientes.setBounds(0, 230, 206, 46);
 		panel_botones.add(boton_Recientes);
 		
-		JButton boton_Nueva_Lista = new JButton("Nueva Lista");
-		boton_Nueva_Lista.setBounds(325, 1, 105, 26);
+		boton_Recientes.addMouseListener(new MouseAdapter() {
+		    public void mouseEntered(MouseEvent evt) {
+		    	boton_Recientes.setContentAreaFilled(true);
+		    	boton_Recientes.setBackground(new Color(41,41,41));
+		    }
+
+		    public void mouseExited(MouseEvent evt) {
+		    	boton_Recientes.setContentAreaFilled(false);		    
+		    }
+		});
+		
+		JButton boton_Nueva_Lista = new JButton("        Nueva Lista");
+		boton_Nueva_Lista.setMargin(new Insets(2, 25, 2, 14));
+		boton_Nueva_Lista.setOpaque(false);
+		boton_Nueva_Lista.setRequestFocusEnabled(false);
+		boton_Nueva_Lista.setHorizontalAlignment(SwingConstants.LEFT);
+		boton_Nueva_Lista.setContentAreaFilled(false);
+		boton_Nueva_Lista.setBorderPainted(false);
+		boton_Nueva_Lista.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/src/main/resources/NewListIcon.png")));
+		boton_Nueva_Lista.setForeground(Color.WHITE);
+		boton_Nueva_Lista.setFont(new Font("Lato Black", Font.BOLD, 15));
+		boton_Nueva_Lista.setBounds(0, 275, 206, 46);
 		panel_botones.add(boton_Nueva_Lista);
 		
-		JPanel panel_explorar = new JPanel();
-		panel_explorar.setLayout(null);
-		panel_explorar.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_explorar.setBackground(SystemColor.menu);
-		panel_explorar.setBounds(10, 99, 774, 554);
-		getContentPane().add(panel_explorar);
-		panel_explorar.setVisible(true);
+		boton_Nueva_Lista.addMouseListener(new MouseAdapter() {
+		    public void mouseEntered(MouseEvent evt) {
+		    	boton_Nueva_Lista.setContentAreaFilled(true);
+		    	boton_Nueva_Lista.setBackground(new Color(41,41,41));
+		    }
+
+		    public void mouseExited(MouseEvent evt) {
+		    	boton_Nueva_Lista.setContentAreaFilled(false);		    
+		    }
+		});
 		
-		JPanel panel_mis_listas = new JPanel();
-		panel_mis_listas.setLayout(null);
-		panel_mis_listas.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_mis_listas.setBackground(SystemColor.menu);
-		panel_mis_listas.setBounds(10, 99, 774, 554);
-		getContentPane().add(panel_mis_listas);
-		panel_mis_listas.setVisible(false);
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(Login.class.getResource("/src/main/resources/AppBG.png")));
+		label.setBounds(0, 0, 1280, 720);
+		getContentPane().add(label);
 		
-		JPanel panel_recientes = new JPanel();
-		panel_recientes.setLayout(null);
-		panel_recientes.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_recientes.setBackground(SystemColor.menu);
-		panel_recientes.setBounds(10, 99, 774, 554);
-		getContentPane().add(panel_recientes);
-		panel_recientes.setVisible(false);
 		
-		JPanel panel_nueva_lista = new JPanel();
-		panel_nueva_lista.setLayout(null);
-		panel_nueva_lista.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_nueva_lista.setBackground(SystemColor.menu);
-		panel_nueva_lista.setBounds(10, 99, 774, 554);
-		getContentPane().add(panel_nueva_lista);
-		panel_nueva_lista.setVisible(false);
+		this.addMouseListener(new MouseAdapter()			//Para arrastrar la ventana
+				{
+				   public void mousePressed(MouseEvent e)
+				   {
+				      posX = e.getX();
+				      posY = e.getY();
+				   }
+				});
+				
+				this.addMouseMotionListener(new MouseAdapter()
+				{
+				     public void mouseDragged(MouseEvent evt)
+				     {	
+						setLocation(evt.getXOnScreen()-posX,evt.getYOnScreen()-posY);				
+				     }
+				});
+
 	}
 }
